610d81466114cc58838b8b6c8ed825f5ba8ad7b2
risma225 <rissma225@gmail.com>
2023-09-12 11:07:56 +0700

    Upload project

---
+++ proses_login.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+include 'koneksi.php';
+
+$username = $_POST['username'];
+$password = sha1($_POST['password']);
+
+$sql = "SELECT * FROM user WHERE username = '$username' AND password ='$password'";
+$data = mysqli_query($conn, $sql);
+$result =  mysqli_num_rows($data);
+
+if ($result == 1) {
+    // echo "Login berhasil!";
+    $_SESSION['username'] = $username;
+    header('location: index.php');
+} else {
+    echo "Username atau password salah!";
+}
+?>
